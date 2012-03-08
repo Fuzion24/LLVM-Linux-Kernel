fc9a9accef2640cdd8d30a4127f1d0295af61cb0
Mark Charlebois <mcharleb@qualcomm.com>
2012-03-08 11:48:29 -0800

    Finally got the clang opts correct for armv7 cross compile

--- common/arm/bin/clang-wrap.sh
+++ common/arm/bin/clang-wrap.sh
@@ -6,8 +6,7 @@ CLANG_INSTALL_DIR=${TOPDIR}/clang/install-dev
 ARMGCC=`which arm-none-linux-gnueabi-gcc`
 ARMGCCDIR=`dirname $ARMGCC`
 ARMGCCINCLUDE="$ARMGCCDIR"/../arm-none-linux-gnueabi/libc/usr/include
-AS=arm-none-linux-gnueabi-as
 export COMPILER_PATH=/opt/arm-2011.03
-CC="${CLANG_INSTALL_DIR}/bin/clang -v -ccc-host-triple arm-none-linux-gnueabi -ccc-gcc-name arm-none-linux-gnueabi-gcc --sysroot=/opt/arm-2011.03/arm-none-linux-gnueabi/libc"
+CC="${CLANG_INSTALL_DIR}/bin/clang -v -ccc-host-triple arm-none-linux-gnueabi -ccc-gcc-name arm-none-linux-gnueabi-gcc --sysroot=/opt/arm-2011.03/arm-none-linux-gnueabi/libc -march=armv7-a -mfpu=neon"
 
 ${CC} $*
