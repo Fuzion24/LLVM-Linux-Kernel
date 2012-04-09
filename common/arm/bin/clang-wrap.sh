21c06d5535c4d53f461f32210fd2bc3c768a4db5
Mark Charlebois <mcharleb@qualcomm.com>
2012-04-08 21:51:51 -0700

    Removed duplicate patches in MSM that broke the patch process. Removed unneeded environment vars from clang-wrap.sh Updated target kernel config files

--- common/arm/bin/clang-wrap.sh
+++ common/arm/bin/clang-wrap.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 TOPDIR=`dirname $0`/../../..
-export PATH=/opt/arm-2011.03/bin:$PATH
 CLANG_INSTALL_DIR=${TOPDIR}/clang/install
-ARMGCC=`which arm-none-linux-gnueabi-gcc`
-ARMGCCDIR=`dirname $ARMGCC`
-ARMGCCINCLUDE="$ARMGCCDIR"/../arm-none-linux-gnueabi/libc/usr/include
 export COMPILER_PATH=/opt/arm-2011.03
-CC="${CLANG_INSTALL_DIR}/bin/clang -v -ccc-host-triple arm-none-linux-gnueabi -ccc-gcc-name arm-none-linux-gnueabi-gcc --sysroot=${COMPILER_PATH}/arm-none-linux-gnueabi/libc -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
+CC="${CLANG_INSTALL_DIR}/bin/clang -ccc-host-triple arm-none-linux-gnueabi -ccc-gcc-name arm-none-linux-gnueabi-gcc --sysroot=${COMPILER_PATH}/arm-none-linux-gnueabi/libc -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
 
 ${CC} $*
