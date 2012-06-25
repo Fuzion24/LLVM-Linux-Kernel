31ca8153f57757cc9c7fa4399a20e3a6ae416872
Behan Webster <behanw@converseincode.com>
2012-06-25 16:31:50 -0400

    Update run-tests.sh to output LTP results
    Signed-off-by: Behan Webster <behanw@converseincode.com>

--- test/ltp/scripts/run-tests.sh
+++ test/ltp/scripts/run-tests.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+#TESTS=ltplite
 TESTS="fcntl-locktests,filecaps,fs,ipc,math,mm,pipes,pty,quickhit,sched,syscalls,timers"
-TESTS=ltplite
 
 mount /proc
 mount /sys
 cd /opt/ltp
+
 BEFORE=`date`
 echo $BEFORE
+
 ./runltp -p -q -f "$TESTS"
+cat results/*
 
 echo "Start: $BEFORE"
 echo "Finish: `date`"
