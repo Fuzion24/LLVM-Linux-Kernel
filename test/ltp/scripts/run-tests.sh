90c15b0728a70b24fc09dc9cd4a850cd9f422cfd
Behan Webster <behanw@converseincode.com>
2012-06-29 17:22:32 -0400

    Update run-tests.sh script to output test resuls
    * Allow test to be specified at linux kernel /proc/cmdline
    * Provide default list of tests to be run
    * Cat test results
    * Cat test failures
    * Poweroff qemu at end of test run
    
    Signed-off-by: Behan Webster <behanw@converseincode.com>

--- test/ltp/scripts/run-tests.sh
+++ test/ltp/scripts/run-tests.sh
@@ -1,18 +1,43 @@
-#!/bin/sh
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+#!/bin/bash
 
 #TESTS=ltplite
-TESTS="fcntl-locktests,filecaps,fs,ipc,math,mm,pipes,pty,quickhit,sched,syscalls,timers"
+TESTS=${1:-`cat /proc/cmdline | sed -e 's/^.*ltptest=\([^ ]*\).*$/\1/; s/.*=.*//'`}
+TESTS=${TESTS:-fcntl-locktests,filecaps,fs,ipc,mm,pipes,pty,quickhit,sched,syscalls,timers}
 
+echo "Running tests: $TESTS"
+
+# Setup environment
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 mount /proc
 mount /sys
 cd /opt/ltp
 
-BEFORE=`date`
-echo $BEFORE
+# Remove past test results
+rm -f results/*.log output/*.failed 2>/dev/null
 
+# Timing before
+BEFORE=`date +%s`
+date --date "@$BEFORE"
+
+# Run tests
 ./runltp -p -q -f "$TESTS"
-cat results/*
 
-echo "Start: $BEFORE"
-echo "Finish: `date`"
+# Timing after
+AFTER=`date +%s`
+DIFF=$(($AFTER - $BEFORE))
+
+# Display test results
+echo "--- Results --------------------------------------------------------------------"
+cat results/*.log
+echo "--- End Results ----------------------------------------------------------------"
+echo
+echo "Start:  `date --date @$BEFORE` ($BEFORE)"
+echo "Finish: `date --date @$AFTER` ($AFTER)"
+echo "Elapsed: `date --date @$DIFF` ($DIFF)"
+echo
+echo "--- Failures -------------------------------------------------------------------"
+cat output/*.failed 2>/dev/null
+echo "--- End Failures ---------------------------------------------------------------"
+
+# Shutdown
+poweroff -f
