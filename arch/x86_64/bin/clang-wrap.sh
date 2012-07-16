866b1dbbee58210564bcb837d3dc0199f23fbc43
Behan Webster <behanw@converseincode.com>
2012-07-16 14:45:58 -0400

    Simplify clang-wrap for i586 and x86_64
    Signed-off-by: Behan Webster <behanw@converseincode.com>

--- arch/x86_64/bin/clang-wrap.sh
+++ arch/x86_64/bin/clang-wrap.sh
@@ -4,14 +4,6 @@
 
 export PATH=`echo $PATH | sed -E 's/(.*?) :(.*)/\2\1/g; s/(.*?) :(.*)/\2\1/g; s/(.*?) :(.*)/\2\1/g'`
 
-ARMGCC=`which arm-none-linux-gnueabi-gcc`
-export ARMGCCDIR=${CSCC_BINDIR:-`dirname $ARMCC`}
-export COMPILER_PATH=${CSCC_DIR:-`dirname $ARMGCCDIR`}
-export ARMGCCSYSROOT="$COMPILER_PATH/$HOSTTYPE/libc"
-export ARMGCCINCLUDE="$ARMGCCSYSROOT/usr/include"
-export HOSTTYPE=${HOSTTYPE:-x86_64-none-linux-gnueabi}
-export HOSTRIPLE=${HOSTRIPLE:-x86_64-none-gnueabi}
-
 CLANGFLAGS="-march=x86_64 -fcatch-undefined-behavior"
 
 CC="$LLVMINSTALLDIR/bin/clang ${CLANGFLAGS}"
