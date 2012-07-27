c5f7f312a4b9ffd936397cc50170a6aa0796824c
Mark Charlebois <charlebm@gmail.com>
2012-07-27 12:14:46 -0700

    First step to enabling more that codesourcery cross compiler for ARM

--- arch/arm/codesourcery.mk
+++ arch/arm/codesourcery.mk
@@ -21,37 +21,40 @@
 # IN THE SOFTWARE.
 ##############################################################################
 
-# Note: This file must be included after ${TOPDIR}/common.mk
+# Note: use CROSS_ARM_VERSION=codesourcery to include this file
 
-include ${ARCHDIR}/all/all.mk
+export CSCC_DIR
+export CSCC_BINDIR
+export COMPILER_PATH=${CSCC_DIR}
+export HOST_TYPE=${HOST}
+export HOST_TRIPLE
 
-# Configure the requested ARM cross compiler
-# Sets CROSS_GCC, PATH, and COMPILER_DEPS, HOST, HOST_TRIPLE
-# 
-ifeq ($(CROSS_GCC),)
-ifeq ($(CROSS_ARM_VERSION),android)
-include ${ARCHDIR}/arm/android.mk
-else
-include ${ARCHDIR}/arm/codesourcery.mk
-endif
-endif
-
-ARCH_ARM_DIR	= ${ARCHDIR}/arm
-ARCH_ARM_BINDIR	= ${ARCH_ARM_DIR}/bin
-ARCH_ARM_PATCHES= ${ARCH_ARM_DIR}/patches
-ARCH_ARM_TMPDIR	= ${ARCH_ARM_DIR}/tmp
-TMPDIRS		+= ${ARCH_ARM_TMPDIR}
-
-KERNEL_PATCHES	+= $(call add_patches,${ARCH_ARM_PATCHES})
-
-ARCH		= arm
-MAKE_FLAGS	= ARCH=${ARCH}
-MAKE_KERNEL	= ${ARCH_ARM_BINDIR}/make-kernel.sh ${LLVMINSTALLDIR} ${EXTRAFLAGS}
+HOST		= arm-none-linux-gnueabi
+HOST_TRIPLE	= arm-none-gnueabi
 CROSS_COMPILE	= ${HOST}-
-CC		= clang-wrap.sh
-CPP		= ${CC} -E
 
-KERNEL_SIZE_ARTIFACTS	= arch/arm/boot/zImage vmlinux*
+CSCC_URL	= https://sourcery.mentor.com/GNUToolchain/package8739/public/arm-none-linux-gnueabi/arm-2011.03-41-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
+CSCC_NAME	= arm-2011.03
+#CSCC_URL	= https://sourcery.mentor.com/sgpp/lite/arm/portal/package9728/public/arm-none-linux-gnueabi/arm-2011.09-70-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2"
+#CSCC_NAME	= arm-2011.09
 
-# ${1}=Machine_type ${2}=kerneldir ${3}=RAM ${4}=rootfs ${5}=Kernel_opts ${6}=QEMU_opts
-qemu = $(call runqemu,${QEMUBINDIR}/qemu-system-arm,${1},${2}/arch/arm/boot/zImage,${3},${4},${KERNELOPTS} ${5},${QEMUOPTS} ${6})
+CSCC_TAR	= ${notdir ${CSCC_URL}}
+CSCC_DIR	= ${TOOLCHAIN}/${CSCC_NAME}
+CSCC_BINDIR	= ${CSCC_DIR}/bin
+
+# Add path so that ${CROSS_COMPILE}${CC} is resolved
+PATH		+= :${CSCC_BINDIR}:${ARCH_ARM_BINDIR}:
+
+# Get arm cross compiler
+${ARCH_ARM_TMPDIR}/${CSCC_TAR}:
+	@mkdir -p ${ARCH_ARM_TMPDIR}
+	[ -d ${CSCC_DIR} ] || wget -c -P ${ARCH_ARM_TMPDIR} "${CSCC_URL}"
+
+CROSS_GCC=${CSCC_BINDIR}/${CROSS_COMPILE}gcc
+gcc arm-cc: state/cross-gcc
+state/cross-gcc: ${ARCH_ARM_TMPDIR}/${CSCC_TAR}
+	[ -d ${CSCC_DIR} ] || tar -x -j -C ${TOOLCHAIN} -f $<
+	$(call state,$@)
+
+${ARCH_ARM_TMPDIR}:
+	@mkdir -p $@
