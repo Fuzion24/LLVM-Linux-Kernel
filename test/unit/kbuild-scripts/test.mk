1a583194d75d5222c58ce574d1b493ced5d55807
Mark Charlebois <charlebm@gmail.com>
2012-09-11 13:01:03 -0700

    Added tests to Kbuild.include unit test for unsupported Clang flags. Clang still returns true for cc-option even for unsupported flags

--- test/unit/kbuild-scripts/test.mk
+++ test/unit/kbuild-scripts/test.mk
@@ -12,5 +12,12 @@ all:
 	@echo "unused-but-set-variable: " $(call cc-option,-Wno-unused-but-set-variable,)
 	@echo "foo: " $(call cc-option,-Wfoo,)
 	@echo "Disable unused-but-set-variable: " $(call cc-disable-warning, unused-but-set-variable)
+	@echo "--------------"
+	@echo "Testing for unsupported clang flags, these should be blank:"
+	@echo "no-delete-pointer-checks:" $(call cc-option,-fno-delete-pointer-checks,)
+	@echo "conserve-stack:" $(call cc-option,-fconserve-stack,)
+	@echo "delete-null-pointer-checks:" $(call cc-option,-fdelete-null-pointer-checks,)
+	@echo "no-inline-functions-called-once:" $(call cc-option,-fno-inline-functions-called-once,)
+	@echo "no-inline-functions-called-once:" $(call cc-option,-fdelete-null-pointer-checks,)
 	@echo
 
