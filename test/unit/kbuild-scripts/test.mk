b0a682185da89c2b1d7cfb58ef3c095245853fc2
Mark Charlebois <charlebm@gmail.com>
2012-09-12 13:29:26 -0700

    Enhanced Kbuild.include unit test for proposed fixes

--- test/unit/kbuild-scripts/test.mk
+++ test/unit/kbuild-scripts/test.mk
@@ -1,5 +1,3 @@
-include Kbuild.include
-
 seperator = "---------------------------------------------------------------------"
 banner  = ( echo ${seperator}; echo ${1}; echo ${seperator} )
 
@@ -8,13 +6,19 @@ all:
 	@$(CC) --version
 	@echo "-------------"
 	@echo "cc-version: " $(call cc-version,)
-	@echo "no-reorder-blocks: " $(call cc-option,-fno-reorder-blocks,)
-	@echo "unused-but-set-variable: " $(call cc-option,-Wno-unused-but-set-variable,)
-	@echo "foo: " $(call cc-option,-Wfoo,)
-	@echo "Disable unused-but-set-variable: " $(call cc-disable-warning, unused-but-set-variable)
 	@echo "--------------"
-	@echo "Testing for unsupported clang flags, these should be blank:"
+	@echo "Testing for supported flags, these should not be blank for both clang and gcc:"
+	@echo "supported warning: " $(call cc-disable-warning,error,)
+	@echo "supported warning: " $(call cc-disable-warning,unused-variable,)
+	@echo "supported flag: " $(call cc-option,-fno-common,)
+	@echo "--------------"
+	@echo "Testing for unsupported flags, these should be blank for both clang and gcc:"
+	@echo "unsupported flag (foo): " $(call cc-option,-ffoo,)
+	@echo "unsupported warning (bar): " $(call cc-option,-Wbar,)
+	@echo "--------------"
+	@echo "Testing for unsupported clang flags, these should be blank for clang, not for gcc:"
 	@echo "no-delete-pointer-checks:" $(call cc-option,-fno-delete-pointer-checks,)
+	@echo "unused-but-set-variable: " $(call cc-disable-warning,unused-but-set-variable,)
 	@echo "conserve-stack:" $(call cc-option,-fconserve-stack,)
 	@echo "delete-null-pointer-checks:" $(call cc-option,-fdelete-null-pointer-checks,)
 	@echo "no-inline-functions-called-once:" $(call cc-option,-fno-inline-functions-called-once,)
