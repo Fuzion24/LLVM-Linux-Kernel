e2f2394e4e35b36f0d2ec9afae8dbd7cc581132a
Behan Webster <behanw@converseincode.com>
2012-10-01 14:14:19 -0400

    Remove unused KERNEL_PATCHES make variable
    Signed-off-by: Behan Webster <behanw@converseincode.com>

--- arch/arm/arm.mk
+++ arch/arm/arm.mk
@@ -32,7 +32,6 @@ RAZE_TARGETS	+= arm-cc-clean
 
 include ${ARCHDIR}/arm/toolchain/toolchain.mk
 
-KERNEL_PATCHES	+= $(call add_patches,${ARCH_ARM_PATCHES})
 KERNEL_PATCH_DIR+= ${ARCH_ARM_PATCHES} ${ARCH_ARM_PATCHES}/${KERNEL_REPO_PATCHES}
 
 HELP_TARGETS	+= arm-help
