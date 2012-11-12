3976101e32650a3e28bdd07e13c34916b94d5bad
Behan Webster <behanw@converseincode.com>
2012-11-12 22:25:35 +0100

    Add test code for all different vlais solutions
    Allows you to test all versions of the VLAIS code at once.
    
    Signed-off-by: Behan Webster <behanw@converseincode.com>

--- test/unit/vlais/test.c
+++ test/unit/vlais/test.c
@@ -5,26 +5,41 @@ extern int vlais(int a, int b, int c, int d);
 extern int novlais_gcc(int a, int b, int c, int d);
 extern int novlais_clang(int a, int b, int c, int d);
 
+#define QUOTEME(x) #x
+#define vlais_args 1, 2, 3, 3
+#define NO_VLAIS_TEST(name) \
+extern int name ## _gcc(int, int, int, int); \
+extern int name ## _clang(int, int, int, int); \
+void test_ ## name (int soln) { \
+	long ret = name ## _gcc(vlais_args); \
+	if (ret != soln) { \
+		printf("FAIL: " QUOTEME(name) " doesn't work with gcc\n"); \
+		exit(1); \
+	} else { \
+		printf("PASS: " QUOTEME(name) " with gcc\n"); \
+	} \
+\
+	ret = name ## _clang(vlais_args); \
+	if (ret != soln) { \
+		printf("FAIL: " QUOTEME(name) " doesn't work with clang\n"); \
+		exit(1); \
+	} else { \
+		printf("PASS: " QUOTEME(name) " with clang\n"); \
+	} \
+}
+
+NO_VLAIS_TEST(novlais)
+NO_VLAIS_TEST(novlais1)
+NO_VLAIS_TEST(novlais2)
+
 int main(void)
 {
-	long test1 = vlais(1, 2, 3, 3);
+	long test1 = vlais(vlais_args);
 	printf("PASS: vlais with gcc\n");
 
-	long test2 = novlais_gcc(1, 2, 3, 3);
-	if (test1 != test2) {
-		printf("E: no-vlais.c doesn't work with gcc\n");
-		exit(1);
-	} else {
-		printf("PASS: no-vlais with gcc\n");
-	}
-	
-	long test3 = novlais_clang(1, 2, 3, 3);
-	if (test1 != test3) {
-		printf("E: no-vlais.c doesn't work with clang\n");
-		exit(1);
-	} else {
-		printf("PASS: no-vlais with clang\n");
-	}
-	
+	test_novlais(test1);
+	test_novlais1(test1);
+	test_novlais2(test1);
+
 	return 0;
 }
