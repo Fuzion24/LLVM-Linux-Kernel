fc28d9f19180cdba4603bb62a6c3eec52e03470d
Jan-Simon Möller <dl9pf@gmx.de>
2013-01-08 21:11:22 +0100

    Remove vnc support from qemu.

--- test/qemu/qemu.mk
+++ test/qemu/qemu.mk
@@ -87,7 +87,7 @@ ${QEMUSTATE}/qemu-configure: ${QEMUSTATE}/qemu-patch
 	@$(call banner, "Configure QEMU...")
 	@mkdir -p ${QEMUBUILDDIR}
 	(cd ${QEMUBUILDDIR} && ${QEMUSRCDIR}/configure \
-		--target-list=arm-softmmu,mips-softmmu,i386-softmmu,x86_64-softmmu --disable-kvm \
+		--target-list=arm-softmmu,mips-softmmu,i386-softmmu,x86_64-softmmu --disable-kvm --disable-vnc \
 		--disable-sdl --audio-drv-list="" --audio-card-list="" --enable-fdt \
 		--disable-docs --prefix=${QEMUINSTALLDIR})
 	$(call state,$@,qemu-build)
