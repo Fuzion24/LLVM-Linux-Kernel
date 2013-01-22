c966dcfbae17fd54b95c6deb7ece9fc4d8b5c766
Behan Webster <behanw@converseincode.com>
2013-01-21 23:40:35 -0500

    Clean up kbuild test scripts
    Signed-off-by: Behan Webster <behanw@converseincode.com>

--- test/unit/kbuild-scripts/unittests.mk
+++ test/unit/kbuild-scripts/unittests.mk
@@ -1,10 +1,18 @@
+#================================================================================
+# Passed in as a variable
 include $(KBUILD)
 
-# gcc clang
+#================================================================================
+# Test results: 4 possible test output.
+# rule: # gcc/Kbuild.incude clang/Kbuild.incude gcc/Kbuild.patched clang/Kbuild.patched
+# All 4 need to be correct for the rule to PASS. Otherwise FAIL.
 
+#================================================================================
+# These numbers may change as gcc and clang gets updated
 cc-version: # 0407 0402 0407 0402
 	@echo $(call cc-version,)
 
+#================================================================================
 # Testing for supported flags, these should not be blank for both clang and gcc:
 cc-disable-warning-error: # -Wno-error -Wno-error -Wno-error -Wno-error
 	@echo $(call cc-disable-warning,error,)
@@ -13,27 +21,30 @@ cc-disable-warning-unused-variable: # -Wno-unused-variable -Wno-unused-variable 
 cc-option-fno-common: # -fno-common -fno-common -fno-common -fno-common
 	@echo $(call cc-option,-fno-common,)
 
+#================================================================================
 # Testing for unsupported flags, these should be blank for both clang and gcc:
-unsupported-flag-foo: # '' -ffoo '' ''
+cc-option-unsupported-flag-foo: # '' -ffoo '' ''
 	@echo $(call cc-option,-ffoo,)
-unsupported-warning-bar: # '' -Wbar '' ''
+cc-option-unsupported-warning-bar: # '' -Wbar '' ''
 	@echo $(call cc-option,-Wbar,)
 
+#================================================================================
 # Testing for unsupported clang flags, these should be blank for clang, not for gcc:
-unused-but-set-variable: # -Wno-unused-but-set-variable -Wno-unused-but-set-variable -Wno-unused-but-set-variable ''
+cc-disable-warning-unused-but-set-variable: # -Wno-unused-but-set-variable -Wno-unused-but-set-variable -Wno-unused-but-set-variable ''
 	@echo $(call cc-disable-warning,unused-but-set-variable,)
-no-delete-pointer-checks: # -fno-delete-null-pointer-checks -fno-delete-null-pointer-checks -fno-delete-null-pointer-checks ''
+cc-option-no-delete-pointer-checks: # -fno-delete-null-pointer-checks -fno-delete-null-pointer-checks -fno-delete-null-pointer-checks ''
 	@echo $(call cc-option,-fno-delete-null-pointer-checks,)
-conserve-stack: # -fconserve-stack -fconserve-stack -fconserve-stack ''
+cc-option-conserve-stack: # -fconserve-stack -fconserve-stack -fconserve-stack ''
 	@echo $(call cc-option,-fconserve-stack,)
-delete-null-pointer-checks: # -fdelete-null-pointer-checks -fdelete-null-pointer-checks -fdelete-null-pointer-checks ''
+cc-option-delete-null-pointer-checks: # -fdelete-null-pointer-checks -fdelete-null-pointer-checks -fdelete-null-pointer-checks ''
 	@echo $(call cc-option,-fdelete-null-pointer-checks,)
-no-inline-functions-called-once: # -fno-inline-functions-called-once -fno-inline-functions-called-once -fno-inline-functions-called-once ''
+cc-option-no-inline-functions-called-once: # -fno-inline-functions-called-once -fno-inline-functions-called-once -fno-inline-functions-called-once ''
 	@echo $(call cc-option,-fno-inline-functions-called-once,)
 
+#================================================================================
 # Testing for unsupported clang gcc, these should be blank for gcc, not for clang:
-unused-argument: # '' -Qunused-arguments '' -Qunused-arguments
+cc-option-unused-argument: # '' -Qunused-arguments '' -Qunused-arguments
 	@echo $(call cc-option,-Qunused-arguments,)
-unused-as-argument: # '' -Qunused-arguments '' -Qunused-arguments
+as-option-unused-as-argument: # '' -Qunused-arguments '' -Qunused-arguments
 	@echo $(call as-option,-Qunused-arguments,)
 
