be279498a6948178d1d638a946c1eb25b824cfb6
Behan Webster <behanw@converseincode.com>
2013-07-13 23:47:13 -0400

    Change clang-wrap.sh to use $CLANG instead of $LLVMINSTALLDIR
    Signed-off-by: Behan Webster <behanw@converseincode.com>

--- arch/arm/bin/clang-wrap.sh
+++ arch/arm/bin/clang-wrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# PATH, LLVMINSTALLDIR, and HOST_TYPE are exported from the Makefile
+# PATH, CLANG, and HOST_TYPE are exported from the Makefile
 
 #export PATH=`echo $PATH | sed -E 's/(.*?) :(.*)/\2\1/g; s/(.*?) :(.*)/\2\1/g; s/(.*?) :(.*)/\2\1/g'`
 
@@ -9,6 +9,6 @@ export ARMGCCINCLUDE="${ARMGCCSYSROOT}/usr/include"
 
 CLANGFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=neon -fsanitize=undefined-trap -fsanitize-undefined-trap-on-error"
 
-CC="${LLVMINSTALLDIR}/bin/clang -target ${HOST_TRIPLE} -ccc-gcc-name ${HOST}-gcc --sysroot=${ARMGCCSYSROOT} ${CLANGFLAGS}"
+CC="${CLANG} -target ${HOST_TRIPLE} -ccc-gcc-name ${HOST}-gcc --sysroot=${ARMGCCSYSROOT} ${CLANGFLAGS}"
 
 ${CC} $*
