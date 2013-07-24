40f54c6bd66cbfa1ea75628412604f3991f7dbc9
Jan-Simon Möller <dl9pf@gmx.de>
2013-07-24 16:15:59 +0200

    testkernel.sh support now also CLANG_TOOLCHAIN= and skipping revisions.
    Signed-off-by: Jan-Simon Möller <dl9pf@gmx.de>

--- targets/x86_64_tiny/testkernel.sh
+++ targets/x86_64_tiny/testkernel.sh
@@ -7,13 +7,13 @@ while $RUNNING; do
     
     # for old kernels ... we deprecated that patch already.
     sed -i -e "s#extern inline#static inline#g" src/linux/drivers/gpu/drm/i915/i915_drv.h
-    make all test-kill
+    make $1 all test-kill
     ret=$?
     echo "ret = $ret"
 
     # revert above hack again.
     pushd src/linux ; git checkout -- drivers/gpu/drm/i915/i915_drv.h; popd
-    echo "worked [y/N]" ?
+    echo "worked [y/N/s]" ?
     read aw
     if test x"$aw" == x"" -o x"$aw" == x"n" -o x"$aw" == x"N" ; then
 	export ret=1
@@ -23,13 +23,23 @@ while $RUNNING; do
 	export ret=0 
     fi
 
-    if test x"$ret" == x"0" ; then
-	echo good
-	make kernel-bisect-good 2>&1 | tee bisectlog
-    else 
-	echo bad
-	make kernel-bisect-bad 2>&1 | tee bisectlog
+    if test x"$aw" == x"s" -o x"$aw" == x"S" ; then
+	export ret=s
     fi
+
+    if test x"$ret" == x"s" ; then
+	make kernel-bisect-skip 2>&1 | tee -a bisectlog
+    else
+    
+	if test x"$ret" == x"0" ; then
+	    echo good
+	    make kernel-bisect-good 2>&1 | tee -a bisectlog
+	else 
+	    echo bad
+	    make kernel-bisect-bad 2>&1 | tee -a bisectlog
+	fi
+    fi
+
     rm initramfs.img.gz.clang -f 2>&1 > /dev/null
 
     grep -i "first good commit" bisectlog && export RUNNING=false
