3605a9b224d0c52032ae53fc1bf58ad11454e849
Mark Charlebois <charlebm@gmail.com>
2013-10-25 15:29:08 -0700

    Updated unit test for merged globals with a case that succeeds for gcc but fails for clang
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- test/unit/mergedglobal/foo.c
+++ test/unit/mergedglobal/foo.c
@@ -6,17 +6,18 @@ struct pernet_operations
 {
     int init
 };
-static struct notifier_block a = {.notifier_call = 1 };
+static struct notifier_block rtnetlink_dev_notifier = {.notifier_call = 1 };
 
 register_pernet_subsys (struct pernet_operations *);
 register_netdevice_notifier (struct notifier_block *);
 __attribute__ ((__section__ (".init.text"))) rtnetlink_net_init ()
 {
 }
-static struct pernet_operations b = {.init = rtnetlink_net_init };
-
+static struct pernet_operations rtnetlink_net_ops = {.init =
+        rtnetlink_net_init
+};
 rtnetlink_init ()
 {
-    register_pernet_subsys (&b);
-    register_netdevice_notifier (&a);
+    register_pernet_subsys (&rtnetlink_net_ops);
+    register_netdevice_notifier (&rtnetlink_dev_notifier);
 }
