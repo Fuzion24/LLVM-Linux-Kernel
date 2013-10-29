acd30a1da5cabc4e1f202a3a639027f6bf312469
Mark Charlebois <charlebm@gmail.com>
2013-10-29 14:04:01 -0700

    Enhanced unit tests for aarch64
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- test/unit/aarch64/t3.c
+++ test/unit/aarch64/t3.c
@@ -8,9 +8,19 @@ int main()
 
 	asm volatile(
 	"	mrs	%0, mair_el1\n"
+	"	bfi	%0, %1, %2, #8\n"
+	"	msr	mair_el1, %0\n"
+	"	isb\n"
+	: "=&r" (tmp)
+	: "r" (foo), "i" (MT_NORMAL * 8));
+
+#if 0 // This fails for clang
+	asm volatile(
+	"	mrs	%0, mair_el1\n"
 	"	bfi	%0, %1, #%2, #8\n"
 	"	msr	mair_el1, %0\n"
 	"	isb\n"
 	: "=&r" (tmp)
 	: "r" (foo), "i" (MT_NORMAL * 8));
+#endif
 }
