2dc70c15072e1a8469b65094f58ac03cc86e8f9c
Mark Charlebois <charlebm@gmail.com>
2013-10-28 19:59:34 -0700

    Added unit test for clang inline assembly issues for aarch64
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

---
+++ test/unit/aarch64/t3.c
@@ -0,0 +1,16 @@
+#include "common.h"
+
+#define MT_NORMAL 4
+
+int main()
+{
+	u64 foo, tmp;
+
+	asm volatile(
+	"	mrs	%0, mair_el1\n"
+	"	bfi	%0, %1, #%2, #8\n"
+	"	msr	mair_el1, %0\n"
+	"	isb\n"
+	: "=&r" (tmp)
+	: "r" (foo), "i" (MT_NORMAL * 8));
+}
