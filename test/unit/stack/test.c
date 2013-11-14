d9554ec92e4bfb4ac2cc639b0d544564c0db884a
Mark Charlebois <charlebm@gmail.com>
2013-11-13 17:50:25 -0800

    Cleaned up test code for __builtin_stack_pointer
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- test/unit/stack/test.c
+++ test/unit/stack/test.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 
-#define xxx() 
 int main()
 {
-#if 0
-	register unsigned long r13 asm("r13");
-	asm("" : "=r"(r13));
-	register unsigned long foo asm("sp");
-        asm("" : "=r"(foo));
-	printf("sp = %p\n", r13);
-#endif
-
 	printf("sp = %p\n", __builtin_stack_pointer());
 }
