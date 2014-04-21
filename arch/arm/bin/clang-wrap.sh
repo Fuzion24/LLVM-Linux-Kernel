6859d83464e963f966c7466c590a6c556012febd
Mark Charlebois <charlebm@gmail.com>
2014-04-21 11:07:55 -0700

    Upadated arm/bin/clang-wrap.sh to work for codesourcery compiler
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- arch/arm/bin/clang-wrap.sh
+++ arch/arm/bin/clang-wrap.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# PATH, CLANG, and HOST_TYPE are exported from the Makefile
+CWD=`dirname $0`
+TOPDIR=`readlink -f ${CWD}/../../../`
+CLANG=${TOPDIR}/toolchain/clang/install/bin/clang
 
-#export PATH=`echo $PATH | sed -E 's/(.*?) :(.*)/\2\1/g; s/(.*?) :(.*)/\2\1/g; s/(.*?) :(.*)/\2\1/g'`
+# CODESORCERY SETTINGS
+HOST_TRIPLE=arm-none-linux-gnueabi
+ARMGCC_DIR=${TOPDIR}/arch/arm/toolchain/codesourcery/arm-2013.05
+ARMGCC=${TOPDIR}/arch/arm/toolchain/codesourcery/arm-2013.05/bin/${HOST_TRIPLE}-gcc
+[ ! -f ${ARMGCC} ] && echo "codesourcery arm-2013.05 compiler not installed" && exit 1
+ARMGCCSYSROOT=`${ARMGCC} -print-sysroot`
 
-export ARMGCCSYSROOT="${COMPILER_PATH}/${HOST}/libc"
-export ARMGCCINCLUDE="${ARMGCCSYSROOT}/usr/include"
 
 CLANGFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=neon -fsanitize=undefined-trap -fsanitize-undefined-trap-on-error"
 
-CC="${CLANG} -target ${HOST_TRIPLE} -ccc-gcc-name ${HOST}-gcc --sysroot=${ARMGCCSYSROOT} ${CLANGFLAGS}"
+CC="${CLANG} -target ${HOST_TRIPLE} -gcc-toolchain ${ARM_GCC_DIR} --sysroot=${ARMGCCSYSROOT} ${CLANGFLAGS}"
 
 ${CC} $*
