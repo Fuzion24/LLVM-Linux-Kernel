34cc3f3ce10a03e0a7ce7dbb89bd55d09557d7b0
Mark Charlebois <charlebm@gmail.com>
2014-04-21 10:17:27 -0700

    Updated unit test for support of register unsigned long int sp asm("sp")
    Even with Renato's named register patch applied, this is not yet supported.
    
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- test/unit/register-var/test.c
+++ test/unit/register-var/test.c
@@ -4,7 +4,11 @@
 
 int main()
 {
+	register unsigned long int sp asm("sp");
 	register unsigned long int sp2;
+
+	// Test the GCC extension
+	printf("sp = %lu\n", sp);
 
 	get_sp(sp2);
 	printf("sp = %lu\n", sp2);
