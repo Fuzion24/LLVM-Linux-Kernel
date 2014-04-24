b970606ad6019a204e135e77fac24fe0cd66fe7c
Mark Charlebois <charlebm@gmail.com>
2014-04-24 15:20:36 -0700

    Added unit test to debug warning with named register var
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- test/unit/register-var/test.c
+++ test/unit/register-var/test.c
@@ -1,18 +1,6 @@
-#include <stdio.h>
-
-#define get_sp(var) asm ("mov %0, r13" : "=r" (var))
-
 int main()
 {
+	int x = 1;
 	register unsigned long int sp asm("sp");
-	register unsigned long int sp2;
-
-	// Test the GCC extension
-	printf("sp = %lu\n", sp);
-
-	get_sp(sp2);
-	printf("sp = %lu\n", sp2);
-
-	return 0;
 }
 
