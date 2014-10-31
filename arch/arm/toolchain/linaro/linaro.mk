f6fda9f1c9d3beb8a2accd8ce6f394e3f0b1b872
Mark Charlebois <charlebm@gmail.com>
2014-10-31 13:27:11 -0700

    Updated linaro.mk to use x86_64 binary and GCC version 4.9
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- arch/arm/toolchain/linaro/linaro.mk
+++ arch/arm/toolchain/linaro/linaro.mk
@@ -26,25 +26,28 @@
 
 TARGETS		+= linaro-gcc
 
-LINARO_VERSION		?= 13.12
-LINARO_GCC_VERSION	?= 4.8
-LINARO_CC_NAME		?= gcc-linaro-arm-linux-gnueabihf
-LINARO_CC_URL		?= http://releases.linaro.org/${LINARO_VERSION}/components/toolchain/binaries/${LINARO_CC_NAME}-${LINARO_GCC_VERSION}-20${LINARO_VERSION}_linux.tar.bz2
+http://releases.linaro.org/14.10/components/toolchain/binaries/arm-linux-gnueabihf/gcc-linaro-4.9-2014.10-x86_64_arm-linux-gnueabihf.tar.xz
+LINARO_VERSION		?= 14.10
+LINARO_GCC_VERSION	?= 4.9
+LINARO_CC_NAME		?= arm-linux-gnueabihf
+LINARO_CC_DIR_NAME	= gcc-linaro-${LINARO_GCC_VERSION}-20${LINARO_VERSION}-x86_64_${LINARO_CC_NAME}
+LINARO_CC_DIR		= ${LINARO_DIR}/${LINARO_CC_DIR_NAME}
+LINARO_CC_URL		?= http://releases.linaro.org/${LINARO_VERSION}/components/toolchain/binaries/${LINARO_CC_NAME}/${LINARO_CC_DIR_NAME}.tar.xz
 LINARO_DIR		?= ${ARCH_ARM_TOOLCHAIN}/linaro
 LINARO_TMPDIR		= $(call shared,${LINARO_DIR}/tmp)
 TMPDIRS			+= ${LINARO_TMPDIR}
 
-LINARO_CC_TAR		= ${notdir ${LINARO_CC_URL}}
-LINARO_CC_DIR		= ${LINARO_DIR}/${LINARO_CC_NAME}-${LINARO_GCC_VERSION}-20${LINARO_VERSION}_linux
-LINARO_CC_BINDIR	= ${LINARO_CC_DIR}/bin
+LINARO_CC_TAR_XZ	= ${LINARO_CC_DIR_NAME}.tar.xz
+LINARO_CC_TAR		= ${LINARO_CC_DIR_NAME}.tar
+LINARO_CC_BINDIR	= ${LINARO_CC_DIR_NAME}/bin
 
-HOST			?= arm-linux-gnueabihf
+HOST			?= ${LINARO_CC_NAME}
 HOST_TRIPLE		?= ${HOST}
 COMPILER_PATH		= ${LINARO_CC_DIR}
 LINARO_GCC		= ${LINARO_CC_BINDIR}/${CROSS_COMPILE}gcc
 CROSS_GCC		= ${LINARO_GCC}
 
-DEBDEP			+= libstdc++6:i386 libz1:i386 libncurses5:i386
+DEBDEP			+= 
 
 ARM_CROSS_GCC_TOOLCHAIN = ${LINARO_CC_DIR}
 
@@ -55,11 +58,12 @@ PATH			:= ${LINARO_CC_BINDIR}:${PATH}
 ${LINARO_TMPDIR}/${LINARO_CC_TAR}:
 	mkdir -p ${LINARO_TMPDIR}
 	@$(call wget,${LINARO_CC_URL},${LINARO_TMPDIR})
+	(cd ${LINARO_TMPDIR} && unxz ${LINARO_CC_TAR_XZ})
 
 linaro-gcc arm-cc: ${ARCH_ARM_TOOLCHAIN_STATE}/linaro-gcc
 ${ARCH_ARM_TOOLCHAIN_STATE}/linaro-gcc: ${LINARO_TMPDIR}/${LINARO_CC_TAR}
 	rm -rf ${LINARO_CC_DIR}
-	tar -x -j -C ${LINARO_DIR} -f $<
+	tar -x -C ${LINARO_DIR} -f $<
 	$(call state,$@)
 
 state/arm-cc: ${ARCH_ARM_TOOLCHAIN_STATE}/linaro-gcc
